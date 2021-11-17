09b3152cfd8fed74f841667b7f14f7dffad7e092
serty04 <92983939+serty04@users.noreply.github.com>
2021-11-17 14:31:33 +0100

    :):):):):)):):):)):):)
    prøva å få pasiv dekk income

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 var antall_dekk = 0;
-var per_dekk_klikk = 0;
+var per_dekk_klikk = 1;
 var shop_felg = false;
 var shop_wide = false;
 var shop_pigg = false;
 var shop_thick = false;
+var shop_duble = false;
+var shop_tareng = false;
+var shop_pasiv = false;
+
 function dekk(){
- antall_dekk = antall_dekk + 1 + per_dekk_klikk;
+ antall_dekk += per_dekk_klikk;
 
  document.getElementById("score").innerHTML = antall_dekk;
  
 }
-function oppgradering1(){
 
-}
+setInterval(function(){
+    document.getElementById("score").innerHTML = antall_dekk;
+    //clicks++;
+}, 100);
+
+setInterval(function shop(type){
+    document.getElementById("score").innerHTML= antall_dekk
+    if (type === "pasiv") {
+        if (shop_pasiv) {
+            window.alert('You already own this item')
+        } else {
+            if (antall_dekk >= 2000) {
+                per_dekk_klikk = per_dekk_klikk + 30
+                shop_pasiv = true
+                antall_dekk = antall_dekk - 2000
+                window.alert('you bought a new sick rims for youre tire!!!')
+            } else {
+                window.alert('You dont have enough tires')
+            }
+        }
+    }
+}, 100);
+
 /*fra Zakaria*/
 function bigImg(x) {
     x.style.height = "420px";
@@ -43,10 +68,10 @@ function shop(type) {
         if (shop_wide) {
             window.alert('You already own this item')
         } else {
-            if (antall_dekk >= 300) {
+            if (antall_dekk >= 600) {
                 per_dekk_klikk = per_dekk_klikk + 15
                 shop_wide = true
-                antall_dekk = antall_dekk - 300
+                antall_dekk = antall_dekk - 600
                 window.alert('you bought a new wide tire!!')
             } else {
                 window.alert('You dont have enough tires')
@@ -57,10 +82,10 @@ function shop(type) {
         if (shop_pigg) {
             window.alert('You already own this item')
         } else {
-            if (antall_dekk >= 600) {
+            if (antall_dekk >= 2000) {
                 per_dekk_klikk = per_dekk_klikk + 30
                 shop_pigg = true
-                antall_dekk = antall_dekk - 500
+                antall_dekk = antall_dekk - 2000
                 window.alert('you bought a new pigg tire!!')
             } else {
                 window.alert('You dont have enough tires')
@@ -71,14 +96,43 @@ function shop(type) {
         if (shop_thick) {
             window.alert('You already own this item')
         } else {
-            if (antall_dekk >= 1500) {
+            if (antall_dekk >= 6000) {
                 per_dekk_klikk = per_dekk_klikk + 50
                 shop_thick = true
-                antall_dekk = antall_dekk - 2000
+                antall_dekk = antall_dekk - 6000
                 window.alert('you bought a sick new thick rim!!')
             } else {
                 window.alert('You dont have enough tires')
             }
         }
     }
+    if (type === "duble") {
+        if (shop_duble) {
+            window.alert('You already own this item')
+        } else {
+            if (antall_dekk >=14000) {
+                per_dekk_klikk = per_dekk_klikk + 100
+                shop_duble = true
+                antall_dekk = antall_dekk - 14000
+                window.alert('you bought a new sick rims for youre tire!!!')
+            } else {
+                window.alert('You dont have enough tires')
+            }
+        }
+    }
+    if (type === "tareng") {
+        if (shop_tareng) {
+            window.alert('You already own this item')
+        } else {
+            if (antall_dekk >= 20000) {
+                per_dekk_klikk = per_dekk_klikk + 200
+                shop_tareng = true
+                antall_dekk = antall_dekk - 20000
+                window.alert('you bought a new sick rims for youre tire!!!')
+            } else {
+                window.alert('You dont have enough tires')
+            }
+        }
+    }
+    
 }
